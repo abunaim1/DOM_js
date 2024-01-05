de443a368af56565031753d8b774b252ca6fe514
Abu Naim <abunaim@Abus-MacBook-Air-2.local>
2024-01-05 12:52:36 +0600

    getElemnt

--- app.js
+++ app.js
@@ -15,3 +15,24 @@ document.getElementById("title").style.color = "red ";
 
 // remove style from a element
 document.getElementById("title").style.removeProperty("color");
+
+var img = document.getElementById("img");
+
+// console.log(img.getAttribute("src"));
+
+img.setAttribute("alt", "Profile picture");
+var gett = img.getAttribute("alt");
+// console.log(gett); 
+
+img.classList.add("testClass");
+// console.log(img)
+
+var hero = document.getElementById("hero");
+// console.log(hero.innerText);
+
+var input = document.getElementById("input");
+// console.log(input.value);
+
+var parent = document.getElementById("parent");
+// console.log(parent.innerHTML);
+
